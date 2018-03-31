c9ac27efafc787e2f0de37c4178cb9d8ebe436ac
Natchanon Charoensuk <inont@hotmail.co.th>
2018-03-31 10:50:37 +0700

    Setup event registration form

--- src/App.js
+++ src/App.js
@@ -7,28 +7,15 @@ class App extends Component {
     return (
       <section className="section">
         <div className="container">
+          <h1 className="title">Evenn Registration Form</h1>
           <div className="field">
             <label className="label">Name</label>
             <div className="control">
               <input className="input" type="text" placeholder="Text input" />
             </div>
-            </div>
+          </div>
 
-            <div className="field">
-            <label className="label">Username</label>
-            <div className="control has-icons-left has-icons-right">
-              <input className="input is-success" type="text" placeholder="Text input" value="bulma" />
-              <span className="icon is-small is-left">
-                <i className="fas fa-user"></i>
-              </span>
-              <span className="icon is-small is-right">
-                <i className="fas fa-check"></i>
-              </span>
-            </div>
-            <p className="help is-success">This username is available</p>
-            </div>
-
-            <div className="field">
+          <div className="field">
             <label className="label">Email</label>
             <div className="control has-icons-left has-icons-right">
               <input className="input is-danger" type="email" placeholder="Email input" value="hello@" />
@@ -40,55 +27,50 @@ class App extends Component {
               </span>
             </div>
             <p className="help is-danger">This email is invalid</p>
-            </div>
+          </div>
 
-            <div className="field">
-            <label className="label">Subject</label>
+          <div className="field">
+            <label className="label">Ticket Type</label>
             <div className="control">
               <div className="select">
                 <select>
-                  <option>Select dropdown</option>
-                  <option>With options</option>
+                  <option>Select type..</option>
+                  <option>Regular - 100THB</option>
+                  <option>Premium - 300THB</option>
                 </select>
               </div>
             </div>
-            </div>
+          </div>
 
-            <div className="field">
-            <label className="label">Message</label>
+          <div className="field">
+            <label className="label">Add food?</label>
             <div className="control">
-              <textarea className="textarea" placeholder="Textarea"></textarea>
+              <label className="radio">
+                <input type="radio" name="question" />
+                <span> Yes</span>
+              </label>
+              <label className="radio">
+                <input type="radio" name="question" />
+                <span> No</span>
+              </label>
             </div>
-            </div>
+          </div>
 
-            <div className="field">
+          <div className="field">
             <div className="control">
               <label className="checkbox">
                 <input type="checkbox" />
-                I agree to the <a href="#">terms and conditions</a>
+                <span> I agree to the <a href="#">terms and conditions</a></span>
               </label>
             </div>
-            </div>
+          </div>
 
-            <div className="field">
+          <div className="field is-grouped">
             <div className="control">
-              <label className="radio">
-                <input type="radio" name="question" />
-                Yes
-              </label>
-              <label className="radio">
-                <input type="radio" name="question" />
-                No
-              </label>
-            </div>
-            </div>
-
-            <div className="field is-grouped">
-            <div className="control">
-              <button className="button is-link">Submit</button>
+              <button className="button is-link">Register</button>
             </div>
             <div className="control">
-              <button className="button is-text">Cancel</button>
+              <button className="button is-text">Reset</button>
             </div>
           </div>
         </div>
